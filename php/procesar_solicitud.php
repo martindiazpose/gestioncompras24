85b5ea0e8af3c6469ba5118c30b9c8f0bd7f5fc6
martindiazpose <martindiazpose@gmail.com>
2024-07-25 15:15:14 -0300

    aasca

--- php/procesar_solicitud.php
+++ php/procesar_solicitud.php
@@ -5,7 +5,7 @@ include 'config/config.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-
+ 
 require '../vendor/autoload.php';
 
 // Función para enviar correo de notificación
